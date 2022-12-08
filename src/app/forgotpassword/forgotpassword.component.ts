b08a0ad581e79b56bb83ab5dbdbd2c6b5900fd81
fullstackdeveloperlive <fullstackdeveloperdev@gmail.com>
2022-12-08 10:14:09 +0530

    Added Forgot password page

--- src/app/forgotpassword/forgotpassword.component.ts
+++ src/app/forgotpassword/forgotpassword.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-forgotpassword',
@@ -7,9 +8,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ForgotpasswordComponent implements OnInit {
 
-  constructor() { }
+  forgotPasswordFrom:FormGroup= new FormGroup({
+    eamil: new FormControl(""),
+    newPassword: new FormControl("",),
+    confirmPassword: new FormControl(""),
+  });
+
+  constructor(private formBulder:FormBuilder) { }
 
   ngOnInit(): void {
+    this.forgotPasswordFrom =this.formBulder.group({
+      eamil: new FormControl("",Validators.required),
+      newPassword: new FormControl("",Validators.required),
+      confirmPassword: new FormControl("",Validators.required),
+    });
   }
 
 }
